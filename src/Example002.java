4a4c3016916582070da0c2d8dc1a85f208f618c5
rot97 <rot97@mail.ru>
2017-07-01 16:33:54 +0200

    Пример работы с переменными

---
+++ src/Example002.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Игорь on 01.07.2017.
+ */
+public class Example002 {
+    public static void main (String[] args){
+        int num = 100;
+        System.out.println("num = " + num);
+
+        num *= 2;
+        System.out.print("num * 2 = ");
+        System.out.println(num);
+    }
+}
