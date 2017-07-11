93aa317d9bcd336e84cc20973c8946a133387bd7
rot97 <rot97@mail.ru>
2017-07-12 00:32:33 +0300

    Пример цикла в стиле for each

---
+++ src/Example006.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Igor on 12.07.2017.
+ */
+public class Example006 {
+    public static void main(String [] args){
+        int [] s = {23, 44, 56, 76, 323};
+        for (int i : s){
+            System.out.println(i + "^2 = " + i*i);
+        }
+    }
+}
